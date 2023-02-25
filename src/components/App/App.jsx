47ac0fb91e948d9936f4b898009d5a0fa0235de3
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-02-25 16:49:00 +0200

    fix contacts list

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,7 +12,12 @@ import { Notification } from 'components/Notification';
 
 export class App extends Component {
   state = {
-  contacts: [ ],
+    contacts: [ 
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
   filter: '',
   name: '',
   number: ''
@@ -76,9 +81,10 @@ export class App extends Component {
   }
 
   render() {
-    const { filter } = this.state;
+    const { filter, contacts } = this.state;
     const filteredContacts = this.getFilteredContacts();
-    const hasContactsInBook = filteredContacts.length !== 0;
+    // const hasContactsInBook = filteredContacts.length !== 0;
+    const hasContactsInBook = contacts.length !== 0;
     return (
       <Container>
         <Section title="Phonebook">
